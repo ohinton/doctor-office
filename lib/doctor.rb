946c66a7caa13adc0bf699938a99cddb4c39eb07
Brett New and Olivia Hinton <brett@>
2016-07-12 11:44:19 -0700

    pass specs for finding patients that have the same doctor assignment

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -6,4 +6,38 @@ class Doctor
     @specialty = attributes.fetch(:specialty)
     @id = attributes.fetch(:id)
   end
+
+  define_singleton_method(:all) do
+    returned_doctors = DB.exec("SELECT * FROM doctors;")
+    doctors = []
+    returned_doctors.each() do |doctor|
+      name = doctor.fetch("name")
+      specialty = doctor.fetch("specialty")
+      id = doctor.fetch("id").to_i()
+      doctors.push(Doctor.new({:name => name, :specialty => specialty, :id => id}))
+    end
+    doctors
+  end
+
+  define_method(:save) do
+    doctor = DB.exec("INSERT INTO doctors (name, specialty) VALUES ('#{@name}', '#{@specialty}') RETURNING id;")
+    @id = doctor.first.fetch('id').to_i()
+  end
+
+  define_method(:==) do |another_doctor|
+    self.name().==(another_doctor.name()).&(self.specialty().==(another_doctor.specialty())).&(self.id().==(another_doctor.id()))
+  end
+
+  define_method(:patients_doctor_assignment) do
+    patients_list = []
+    returned_patient_list = DB.exec("SELECT * FROM patients WHERE doctor_id = #{self.id()};")
+    returned_patient_list.each() do |patient|
+      name = patient.fetch("name")
+      birthday = patient.fetch("birthday")
+      doctor_id = patient.fetch("doctor_id").to_i()
+      patients_list.push(Patient.new({:name => name, :birthday => birthday, :doctor_id => doctor_id, :id => id}))
+    end
+    patients_list
+  end
+
 end
--- spec/doctor_spec.rb
+++ spec/doctor_spec.rb
@@ -15,4 +15,29 @@ describe(Doctor) do
     end
   end
 
+  describe('.all') do
+    it('will remain empty at first') do
+      expect(Doctor.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('will save the doctor info') do
+      test_doctor = Doctor.new({:name => 'Dr. Love', :id => nil, :specialty => 'Family'})
+      test_doctor.save()
+      expect(Doctor.all()).to(eq([test_doctor]))
+    end
+  end
+
+  describe('#patients_doctor_assignment') do
+    it('will display a list of patients assigned to a particular doctor') do
+      test_doctor = Doctor.new({:name => 'Dr. Love', :id => nil, :specialty => 'Family'})
+      test_doctor.save
+      patient1 = Patient.new({:name => 'Bob', :birthday => '1991-01-01', :doctor_id => test_doctor.id()})
+      patient1.save()
+      patient2 = Patient.new({:name => 'Jill', :birthday => '1992-02-02', :doctor_id => test_doctor.id()})
+      patient2.save()
+      expect(test_doctor.patients_doctor_assignment()).to(eq([patient1, patient2]))
+    end
+  end
 end
